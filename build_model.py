0ed5f938fa20490177b0682d56606634b2398a14
Leo King <135327943+theking-leo@users.noreply.github.com>
2024-07-04 19:03:17 +0200

    Update build_model.py

--- build_model.py
+++ build_model.py
@@ -15,4 +15,4 @@ def build_model():
     model.add(Dense(10, activation='softmax'))
     
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    return model
+    return model 
